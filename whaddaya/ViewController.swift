07ef868fa88e2e191a329c888115ea489511d1f4
jiseung <anna.jiseung@gmail.com>
2019-05-19 10:12:44 -0500

    Initial Commit

---
+++ whaddaya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whaddaya
+//
+//  Created by jiseung on 5/19/19.
+//  Copyright © 2019 jiseung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
